63c2a2cd7f81b8ab543aa7c81436394a2b06a7be
Hieu1866 <hieuwv0091@gmail.com>
2026-01-22 20:07:49 +0700

    Update image zoom animation duration

--- app/sections/image-with-text/image.tsx
+++ app/sections/image-with-text/image.tsx
@@ -50,7 +50,7 @@ const variants = cva("h-auto w-full", {
 
 interface ImageWithTextImageProps
   extends VariantProps<typeof variants>,
-    HydrogenComponentProps {
+  HydrogenComponentProps {
   image: WeaverseImage | string;
   imageAspectRatio: ImageAspectRatio;
   ref?: React.Ref<HTMLDivElement>;
@@ -97,7 +97,7 @@ function ImageWithTextImage(props: ImageWithTextImageProps) {
             "h-auto w-full",
             variants({ objectFit }),
             enableImageHover &&
-              "transition-transform duration-300 ease-out group-hover:scale-105",
+            "transition-transform duration-700 ease-out group-hover:scale-105",
           )}
         />
       </div>
